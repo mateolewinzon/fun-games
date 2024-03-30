8c50bc598087e00410e836385ec0915a453d2b7d
Mateo Lewinzon <mateolewinzon@gmail.com>
2024-03-30 20:49:38 -0300

    refactor main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
 
-export default function Home() {
+function GameLink({ path, name }: { path: string; name: string }) {
   return (
-    <section className="flex flex-col gap-6">
-      <h1 className="text-3xl">Fun Games</h1>
-      <Link href="/tictactoe">
-        <div className="flex justify-start w-fit p-2 border border-green-500">
-          <span className="text-green-500">Tic-Tac-Toe</span>
+    <li className="w-max">
+      <Link href={path}>
+        <div className="flex p-2 border-2 border-green-500 rounded">
+          <span className="text-green-500">{name}</span>
         </div>
       </Link>
-    </section>
+    </li>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl">Fun Games</h1>
+        <h3>Some fun classic games to play with friends. 😎</h3>
+      </div>
+      <ul className="flex flex-col gap-4">
+        <GameLink path="/connect-four" name="Connect Four" />
+        <GameLink path="/tic-tac-toe" name="Tic Tac Toe" />
+      </ul>
+    </div>
   );
 }
