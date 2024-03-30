5381d02c65c6fbef436feabaf46f1fe2ba617a04
Mateo Lewinzon <mateolewinzon@gmail.com>
2024-03-30 17:59:35 -0300

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex flex-col gap-6">
-      <h1 className="text-3xl">Welcome to tic-tac-toe</h1>
+      <h1 className="text-3xl">Fun Games</h1>
       <Link href="/tictactoe">
         <div className="flex justify-start w-fit p-2 border border-green-500">
           <span className="text-green-500">Tic-Tac-Toe</span>
